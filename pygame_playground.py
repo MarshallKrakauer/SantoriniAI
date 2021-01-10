de7a847dc5c14c303f17346860dec3ec51861415
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-01-09 19:40:48 -0800

    Button class added

--- pygame_playground.py
+++ pygame_playground.py
@@ -2,6 +2,7 @@
 """pygame interaction with Santorini game."""
 
 import pygame
+import pygame.freetype
 from auto_santorini import Game
 
 # Define some colors
@@ -11,15 +12,67 @@ GREEN = (0, 200, 0)
 RED = (255, 0, 0)
 TEST_COLOR = (100, 100, 100)
 GOLD = (100, 84.3, 0)
-BLUE = (30, 150, 250)
+BLUE = (30, 250, 150)
 
 # Horizontal and vertical start of board
-START_H = 100
-START_V = 100
+BOARD_TOP_EDGE = 100 # SIZE = (800, 600) #(width <-->, height)
+BOARD_LEFT_EDGE = 275
+BUTTON_MEASURES = [BOARD_LEFT_EDGE + 75, BOARD_TOP_EDGE + 350, 120, 60]
+# Left_edge, top_edge, width, height
 
-game1 = Game()
-board = game1.board
+# Set up the pygame environment
+pygame.init()
+SIZE = (800, 600) #(width <-->, height)
+SCREEN = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Santorini")
+font = pygame.font.SysFont('Calibri', 16, True, False)
 
+class Button(pygame.sprite.Sprite):
+    # Curtosy of https://programmingpixels.com/handling-a-title-screen-game-flow-and-buttons-in-pygame.html
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb):
+        self.mouse_over = False  # indicates if the mouse is over the element
+         # default image
+        default_image = create_surface_with_text(
+            text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb)
+
+        # image when hovering over
+        highlighted_image = create_surface_with_text(
+            text=text,
+            font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb)
+
+        self.images = [default_image, highlighted_image]
+
+        self.rectangles = [
+            default_image.get_rect(center=center_position),
+            highlighted_image.get_rect(center=center_position),
+        ]
+
+        # Calls init on parent class
+        super().__init__()
+
+    @property
+    def image(self):
+        return self.images[1] if self.mouse_over else self.images[0]
+
+    @property
+    def rect(self):
+        return self.rectangles[1] if self.mouse_over else self.rectangles[0]
+
+    def update(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.mouse_over = True
+        else:
+            self.mouse_over = False
+    
+    def draw(self):
+        """ Draws element onto a surface."""
+        SCREEN.blit(self.image, self.rect)
+
+def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
+    """ Returns surface with text written on """
+    font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+    surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+    return surface.convert_alpha()
 
 def map_numbers(x_coor, y_coor):
     """
@@ -38,55 +91,60 @@ def map_numbers(x_coor, y_coor):
     area on board that was clicked
 
     """
-    x_1 = (x_coor - 100) // 50
-    y_1 = (y_coor - 100) // 50
+    x_1 = (x_coor - BOARD_LEFT_EDGE) // 50
+    y_1 = (y_coor - BOARD_TOP_EDGE) // 50
     return (x_1, y_1)
-
 
 def check_valid(num):
     """Return true if valid board spot."""
     return -1 < num < 5
 
-
 def check_undo(x_coor, y_coor):
     """Return true if undo button was selected."""
-    return x_coor >= 175 and x_coor <= 290 and y_coor >= 450 and y_coor <= 500
+    return (BOARD_LEFT_EDGE + 75 <= x_coor <= BOARD_LEFT_EDGE + 190
+            and BOARD_TOP_EDGE + 350 <= y_coor <= BOARD_TOP_EDGE + 400)
 
 
 def end_fanfare(color):
     """Produce visual showing who won the game."""
     pygame.draw.rect(SCREEN, GREEN,
-                     [175, 450, 120, 60], 0)
+                     BUTTON_MEASURES, 0)
     text = font.render("WINNER: " + color, True, BLACK)
     SCREEN.blit(text, (175 + 20, 450 + 20))
 
-
 def undo_button():
     """Show undo button when player can select piece."""
-    pygame.draw.rect(SCREEN, (50, 50, 50),
-                     [175, 450, 120, 60], 0)
-    text = font.render("UNDO", True, BLACK)
-    SCREEN.blit(text, (175 + 40, 450 + 20))
-    pygame.draw.rect(SCREEN, RED,
-                     [175, 450, 120, 60], 3)
-
+    x, y = pygame.mouse.get_pos()
+    if check_undo(x,y):
+        undo_font = pygame.font.SysFont('Calibri', 24, True, False)
+        #pygame.draw.rect(SCREEN, (50, 50, 50),
+        #             BUTTON_MEASURES, 0)
+        text = undo_font.render("UNDO", True, BLACK)
+        SCREEN.blit(text, (BUTTON_MEASURES[0] + 45/2, BUTTON_MEASURES[1] + 20))
+    else:
+        undo_font = pygame.font.SysFont('Calibri', 12, True, False)
+        #pygame.draw.rect(SCREEN, (50, 50, 150),
+        #             BUTTON_MEASURES, 0)
+        text = undo_font.render("UNDO", True, BLACK)
+        SCREEN.blit(text, (BUTTON_MEASURES[0] + 45, BUTTON_MEASURES[1] + 20))
+    
+    #pygame.draw.rect(SCREEN, RED,BUTTON_MEASURES, 3)
 
 def show_thinking(dot=3):
     """Show a "THINKING..." box when AI plays turn."""
     elipse = '.' * dot
     pygame.draw.rect(SCREEN, (50, 50, 50),
-                     [175, 450, 120, 60], 0)
+                     BUTTON_MEASURES, 0)
     text = font.render("THINKING" + elipse, True, BLACK)
-    SCREEN.blit(text, (160 + 40, 450 + 20))
+    SCREEN.blit(text, (BUTTON_MEASURES[0] + 20, BUTTON_MEASURES[1] + 20))
 
-
-def draw_board():
+def draw_board(board):
     """Draw the 5x5 game board with player pieces."""
     for i in range(5):
         for j in range(5):
-            V = START_V + 50 * i
-            H = START_H + 50 * j
-            pygame.draw.rect(SCREEN, BLACK,
+            V = BOARD_LEFT_EDGE + 50 * i
+            H = BOARD_TOP_EDGE + 50 * j
+            pygame.draw.rect(SCREEN, WHITE,
                              [V, H, 50, 50], 2)
             # Draw piece or dome
 
@@ -115,65 +173,113 @@ def draw_board():
             SCREEN.blit(text, (V + 20, H + 20))
 
 
-# Set up the pygame environment
+def title_screen():
+    """Show title screen before playing."""
+    
+    start_button = Button(
+        center_position=(200,200),
+        font_size=20,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Hello World",
+    )
+    
+    end_it = False
+    myname = "Santorini"
+    while end_it==False:
+        SCREEN.fill(WHITE)
+        myfont=pygame.font.SysFont("Britannic Bold", 40)
+        nlabel=myfont.render("Welcome "+myname+" Start Screen", 1, (255, 0, 0))
+        start_button.update(pygame.mouse.get_pos())
+        start_button.draw()
+        
+        for event in pygame.event.get():
+            if event.type== pygame.MOUSEBUTTONDOWN:
+                end_it=True
+        SCREEN.blit(nlabel,(800/2,400/2))
+        pygame.display.flip()
+
+def title_alt():
+    pass
+    
+
+def play_game(game):
+    """
+    Create and run UI to play Santorini.
+
+    Parameters
+    ----------
+    game : Game
+        Game object where moves/builds will be game
+
+    """
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+    counter = 0  # not in use, holder if something shows every other frame
+    done = False  # ends pygame when true
+    show_board = False  # Used to show board when AI is playing
+    # -------- Main Program Loop -----------
+    pygame.event.clear()
+    while not done:
+        # --- Main event loop
+        SCREEN.fill(BLUE)
+
+        event = pygame.event.wait()  # event queue
+        for event in pygame.event.get():  # check for end of game
+            if event.type == pygame.QUIT:
+                done = True
+
+        # Special conditions
+        # Check for end of game
+        if game.end:
+            game.end_game()
+            end_fanfare(game.color)  # do something for endgame
+        # Show that AI is "thinking"
+        elif game.turn > 4 and game.color == 'G':
+            show_thinking()
+            if show_board:
+                game.future_moves()
+                show_board = False
+            else:
+                show_board = True
+        # Allow undo during a select action
+        elif game.sub_turn == 'move':
+            undo_button()
+
+        # Allow player to select actions
+        if event.type == pygame.MOUSEBUTTONDOWN and not game.end:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            x, y = map_numbers(pos[0], pos[1])
+            if check_valid(x) and check_valid(y):
+                game.play_turn(x, y)
+            elif check_undo(pos[0], pos[1]) and game.sub_turn == 'move':
+                game.undo()
+
+        # Draw the board after changes have been made
+        board = game.board
+        draw_board(board)
+
+        # --- Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+        # --- Limit to 60 frames per second
+        counter += 1
+        clock.tick(30)
+
+    # Close the window and quit.
+    pygame.quit()
+
 pygame.init()
-SIZE = (450, 600)
+SIZE = (800, 600) #(width <-->, height)
 SCREEN = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Santorini")
-font = pygame.font.SysFont('Calibri', 16, True, False)
 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-counter = 0  # not in use, holder if something shows every other frame
-done = False  # ends pygame when true
-show_board = False  # Used to show board when AI is playing
-# -------- Main Program Loop -----------
-pygame.event.clear()
-while not done:
-    # --- Main event loop
-    SCREEN.fill(BLUE)
+def main():
+    """Play game including title screen."""
+    # Get game board
+    game1 = Game()
+    title_screen()
+    play_game(game1)
 
-    event = pygame.event.wait()  # event queue
-    for event in pygame.event.get():  # check for end of game
-        if event.type == pygame.QUIT:
-            done = True
-
-    # Special conditions
-    # Check for end of game
-    if game1.end:
-        game1.end_game()
-        end_fanfare(game1.color)  # do something for endgame
-    # Show that AI is "thinking"
-    elif game1.turn > 4 and game1.color == 'G':
-        show_thinking()
-        if show_board:
-            game1.future_moves()
-            show_board = False
-        else:
-            show_board = True
-    # Allow undo during a select action
-    elif game1.sub_turn == 'move':
-        undo_button()
-
-    # Allow player to select actions
-    if event.type == pygame.MOUSEBUTTONDOWN and not game1.end:
-        pos = pygame.mouse.get_pos()
-        x, y = map_numbers(pos[0], pos[1])
-        if check_valid(x) and check_valid(y):
-            game1.play_turn(x, y)
-        elif check_undo(pos[0], pos[1]) and game1.sub_turn == 'move':
-            game1.undo()
-
-    # Draw the board after changes have been made
-    board = game1.board
-    draw_board()
-
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    # --- Limit to 60 frames per second
-    counter += 1
-    clock.tick(30)
-
-# Close the window and quit.
-pygame.quit()
+main()
