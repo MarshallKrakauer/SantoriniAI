643fc4fe848880f16be1d54888f8d4fc44150fc1
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-05-21 22:09:12 -0700

    change exploration paramater

--- MCTS.py
+++ MCTS.py
@@ -6,8 +6,8 @@ import datetime as dt
 import random
 from math import sqrt, log
 
-EXPLORATION_FACTOR = 0.5  # square root of 2
-TURN_TIME = 30
+EXPLORATION_FACTOR = 1
+TURN_TIME = 45
 
 # Global variable, stores list of moves with corresponding potential moves
 # Exists to save time from hefty potential moves process
