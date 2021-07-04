a557c448a26f21d2f2621d0e7cb3c303bf8c8df2
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-07-03 17:17:23 -0700

    additional game data

--- tree_model.py
+++ tree_model.py
@@ -58,7 +58,6 @@ if __name__ == '__main__':
     # Setup Data for Prediction Modeling
     df = pd.read_csv('game_list.csv')
     df = df.loc[df.iloc[:, 1] <= (20/60), :]
-    print(df.shape)
     y = df.iloc[:, 0]
     X = df.iloc[:, 1:]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
